3786ddb98c38a91d5a7e4e64b841ac333b8505a9
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-04-20 17:31:05 +0300

    CoreFinalProject Version 1.3

---
+++ test/com/gojavaonline2/group15/corefinalproject/NodeTest.java
@@ -0,0 +1,38 @@
+package com.gojavaonline2.group15.corefinalproject;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+@RunWith(value = Parameterized.class)
+public class NodeTest {
+
+    private int a, b;
+    private int expected;
+
+    public NodeTest(int a, int b, int expected){
+        this.a = a;
+        this.b = b;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "{index}: compareTo({0},{1}={2}")
+
+    public static Iterable<Object[]> data1(){
+        return Arrays.asList(new Object[][]{
+                {1, 1, 0},
+                {2, 2, 0},
+                {8, 2, -1},
+                {4, 5, 1}
+        });
+    }
+
+    @Test
+    public void compareToTest() throws Exception {
+        Tree<Integer> tTree = new Tree<Integer>().add(a);
+        Assert.assertEquals(expected, tTree.getRootNode().compareTo(b));
+    }
+}
